b2f407a8d0d91ef56381d6b7e8d15094093eee56
a.onoda <akira.onoda-n145@nid.co.jp>
2018-01-26 08:53:15 +0900

    Window first impl

---
+++ src/ui/window.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
+
+const WINDOW_OPTIONS = {
+    width: 1024,
+    height: 640,
+    resizable: false,
+    minimizable: false,
+    maximizable: false,
+    fullscreenable: false,
+    center:true,
+    kiosk: true,
+    title: "GistDrive",
+    //icon: TODO,
+    frame: false,
+    //backgroundColor: TODO,
+    darkTheme: true,
+    webPreferences:{
+        // devTools: false,
+        nodeIntegrationInWorker: true,
+        preload: true,
+    },
+}
+
+class Window{
+    constructor(onClose){
+        this.win = null;
+        app.on('ready',this.createWindow);
+        app.on('window-all-closed', ()=>{
+            onClose.call();
+            app.quit();
+        });
+        app.on('active',()=>{
+            if(this.win === null){
+                this.createWindow();
+            }
+        });
+    }
+
+    createWindow(){
+        this.win = new BrowserWindow(WINDOW_OPTIONS);
+        this.win.loadURL(path.join(__dirname,'content','index.html'));
+        this.win.on('closed', ()=>{this.win = null});
+        this.win.webContents.openDevTools();
+    }
+
+}
+
+module.exports = Window;
+
