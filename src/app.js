c1c807fd47dde41944344696d183a9c650f2006b
sAbIz <sabiz@users.noreply.github.com>
2018-01-26 07:41:42 +0900

    Impl connect gist (#1)
    * Support Account Load&save
    * Support Gist Connect

---
+++ src/app.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const Gists = require('gists');
+
+const Config = require('./lib/config');
+const prompt = require('prompt-sync')();//For CLI
+
+const NAME_CONFIG_FILE = 'conf.json';
+const CONFIG_GITHUB_NAME = 'GITHUB_USER_NAME';
+const CONFIG_GITHUB_PASSWORD = 'GITHUB_PASSWORD';
+
+class App {
+    constructor(){
+        this.conf = new Config(NAME_CONFIG_FILE,{autoSave:true});
+        this.gists = {};
+    }
+
+    start() {
+        this.conf.load();
+        let name = this.conf.get(CONFIG_GITHUB_NAME);
+        if(!name){
+            name = prompt('Github user name > ');
+            this.conf.set(CONFIG_GITHUB_NAME, name);
+        }
+        const key = prompt.hide('password Encrypt key > ');
+        let password = this.conf.getAndDecrypt(CONFIG_GITHUB_PASSWORD, key);
+        if(!password){
+            password = prompt.hide('Github password > ');
+            this.conf.setAndEncrypt(CONFIG_GITHUB_PASSWORD, password, key);
+        }
+        let gist = new Gists({
+            username: name,
+            password: password
+        });
+        gist.list({user:name},(err,res)=>{
+            console.log(err,res);
+        });
+    }
+}
+
+module.exports = App;
