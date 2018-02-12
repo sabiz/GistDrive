98e4290cd2355d819f514298110c6a815d5db220
sAbIz <sabiz@users.noreply.github.com>
2018-02-12 19:53:14 +0900

    Impl gui frame (#2)
    * Window first impl
    
    * Add Parcel
    
    * Add title bar
    
    * Impl title bar
    
    * Panel layout fix
    
    * Add List item layout
    
    * Add Markdown Preview

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@
 const Gists = require('gists');
 
 const Config = require('./lib/config');
-const prompt = require('prompt-sync')();//For CLI
+const Window = require('./ui/window');
 
 const NAME_CONFIG_FILE = 'conf.json';
 const CONFIG_GITHUB_NAME = 'GITHUB_USER_NAME';
@@ -13,28 +13,33 @@ class App {
     constructor(){
         this.conf = new Config(NAME_CONFIG_FILE,{autoSave:true});
         this.gists = {};
+        this.window = new Window(this.onClose);
     }
 
     start() {
         this.conf.load();
-        let name = this.conf.get(CONFIG_GITHUB_NAME);
-        if(!name){
-            name = prompt('Github user name > ');
-            this.conf.set(CONFIG_GITHUB_NAME, name);
-        }
-        const key = prompt.hide('password Encrypt key > ');
-        let password = this.conf.getAndDecrypt(CONFIG_GITHUB_PASSWORD, key);
-        if(!password){
-            password = prompt.hide('Github password > ');
-            this.conf.setAndEncrypt(CONFIG_GITHUB_PASSWORD, password, key);
-        }
-        let gist = new Gists({
-            username: name,
-            password: password
-        });
-        gist.list({user:name},(err,res)=>{
-            console.log(err,res);
-        });
+        // let name = this.conf.get(CONFIG_GITHUB_NAME);
+        // if(!name){
+        //     name = prompt('Github user name > ');
+        //     this.conf.set(CONFIG_GITHUB_NAME, name);
+        // }
+        // const key = prompt.hide('password Encrypt key > ');
+        // let password = this.conf.getAndDecrypt(CONFIG_GITHUB_PASSWORD, key);
+        // if(!password){
+        //     password = prompt.hide('Github password > ');
+        //     this.conf.setAndEncrypt(CONFIG_GITHUB_PASSWORD, password, key);
+        // }
+        // let gist = new Gists({
+        //     username: name,
+        //     password: password
+        // });
+        // gist.list({user:name},(err,res)=>{
+        //     console.log(err,res);
+        // });
+    }
+
+    onClose() {
+
     }
 }
 
