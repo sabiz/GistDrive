9cf31b729d6de0af92cf293a240e2e55a0dc2074
a.onoda <akira.onoda-n145@nid.co.jp>
2018-02-15 08:52:00 +0900

    Imple markdown preview

--- src/ui/view/script.js
+++ src/ui/view/script.js
@@ -1,70 +1,76 @@
-'use strict';
-
-const {ipcRenderer} = require('electron');
+const { ipcRenderer } = require('electron');
 const alertify = require('alertifyjs');
-alertify.set('notifier','position', 'top-center');
-//alertify.set('notifier','delay', 1000); DEBUG notify
-
 const titlebar = require('./titlebar/titlebar');
 const preview = require('./preview/preview');
 const list = require('./list/list');
 const channel = require('../channel');
+
+alertify.set('notifier', 'position', 'top-center');
+//alertify.set('notifier','delay', 1000); DEBUG notify
+
 const gists = [];
 
-function render(mdText) {
-    document.getElementById('markdown').innerHTML = preview.preview(mdText);
+function renderMarkdown(mdText) {
+    preview.preview(mdText);
 }
 
 function onItemClick(data) {
-    if(!gists.find((e)=>e.id===data.id)){
-        gists.push({id:data.id})
-        ipcRenderer.send(channel.REQUEST_GIST_ITEM, data.id,data.name);
-    }else {
-        //TODO Render Item
+    const tmpData = gists.find(e => e.id === data.id);
+    if (!tmpData) {
+        gists.push({ id: data.id });
+        ipcRenderer.send(channel.REQUEST_GIST_ITEM, data.id, data.name);
+    } else {
+        renderMarkdown(tmpData.content);
     }
 }
 
-window.addEventListener("load",()=>{
+window.addEventListener('load', () => {
     list.registerItemClick(onItemClick);
 });
 
-ipcRenderer.on(channel.REQUEST_USER_NAME,()=>{
-    alertify.prompt( '', 'Github user name ?', '',
-               (evt, value)=>ipcRenderer.send(channel.REQUEST_USER_NAME, value),
-               ()=>alertify.error('TODO: Cancel')).set('closable', false);
+ipcRenderer.on(channel.REQUEST_USER_NAME, () => {
+    alertify.prompt(
+        '', 'Github user name ?', '',
+        (evt, value) => ipcRenderer.send(channel.REQUEST_USER_NAME, value),
+        () => alertify.error('TODO: Cancel'),
+    ).set('closable', false);
 });
 
-ipcRenderer.on(channel.REQUEST_ENCRYPT_KEY,()=>{
-    alertify.prompt( '', 'Password crypt key ?', '',
-               (evt, value)=>ipcRenderer.send(channel.REQUEST_ENCRYPT_KEY, value),
-               ()=>alertify.error('TODO: Cancel')).set('type', 'password').set('closable', false);
+ipcRenderer.on(channel.REQUEST_ENCRYPT_KEY, () => {
+    alertify.prompt(
+        '', 'Password crypt key ?', '',
+        (evt, value) => ipcRenderer.send(channel.REQUEST_ENCRYPT_KEY, value),
+        () => alertify.error('TODO: Cancel'),
+    ).set('type', 'password').set('closable', false);
 });
 
-ipcRenderer.on(channel.REQUEST_PASSWORD,()=>{
-    alertify.prompt( '', 'Github account password ?', '',
-               (evt, value)=>ipcRenderer.send(channel.REQUEST_PASSWORD, value),
-               ()=>alertify.error('TODO: Cancel')).set('type', 'password').set('closable', false);
+ipcRenderer.on(channel.REQUEST_PASSWORD, () => {
+    alertify.prompt(
+        '', 'Github account password ?', '',
+        (evt, value) => ipcRenderer.send(channel.REQUEST_PASSWORD, value),
+        () => alertify.error('TODO: Cancel'),
+    ).set('type', 'password').set('closable', false);
 });
 
-ipcRenderer.on(channel.REQUEST_UPDATE_LIST,(ev,args)=>{
+ipcRenderer.on(channel.REQUEST_UPDATE_LIST, (ev, args) => {
     list.update(...args);
 });
 
-ipcRenderer.on(channel.REQUEST_GIST_ITEM,(ev,args)=>{
-    if(!args || !args[0]){
+ipcRenderer.on(channel.REQUEST_GIST_ITEM, (ev, args) => {
+    if (!args || !args[0]) {
         return;
     }
     const result = args[0];
     let updateItem;
-    gists.forEach((v)=>{
-        if(v.id === result.id) {
-            v.language = result.language;
-            v.content = result.content;
-            updateItem = v;
+    gists.forEach((v, i) => {
+        if (v.id === result.id) {
+            gists[i].language = result.language;
+            gists[i].content = result.content;
+            updateItem = gists[i];
         }
     });
-    console.log(gists);
+    renderMarkdown(updateItem.content);
 });
 
-ipcRenderer.on(channel.SHOW_ERROR,(ev,args)=>alertify.error(args[0]));
+ipcRenderer.on(channel.SHOW_ERROR, (ev, args) => alertify.error(args[0]));
 
